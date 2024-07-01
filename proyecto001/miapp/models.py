3e24e8d7e7bcc03ee0bffb22f09abf872d1625ec
Carlos P <2123110286@untels.edu.pe>
2024-07-01 16:56:10 -0500

    Parte 2 - Tabla Estudiante - OK

--- proyecto001/miapp/models.py
+++ proyecto001/miapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Estudiante(models.Model):
+    idestudiante = models.AutoField(primary_key=True)
+    codigo = models.CharField(max_length=10, unique=True, verbose_name="Codigo")
+    dni = models.CharField(max_length=9, unique=True, verbose_name="dni")
+    nombre = models.CharField(max_length=50, verbose_name="Nombre")
+    ap_mat = models.CharField(max_length=50, verbose_name="Ap_pat")
+    ap_pat = models.CharField(max_length=50, verbose_name="Ap_mat")
+    direccion = models.CharField(max_length=150, verbose_name="Direccion")
+    telefono = models.CharField(max_length=10, unique=True, verbose_name="Telefono")
+    estado = models.BooleanField(default=True, verbose_name="Estado")
